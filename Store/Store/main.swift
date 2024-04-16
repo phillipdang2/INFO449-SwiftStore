5bc8feb4d11eb97a3ce42c7e14b972890f3969e3
phillip <pdang2@uw.edu>
2024-04-15 22:51:09 -0700

    finished hw

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,13 +7,73 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    var name: String { get }
+    func price() -> Int
+}
 
-class Item {}
+class Item: SKU {
+    var name: String
+    var priceEach: Int
+    
+    init(name: String, priceEach: Int) {
+        self.name = name
+        self.priceEach = priceEach
+    }
+    
+    func price() -> Int {
+        return self.priceEach
+    }
+}
 
-class Receipt {}
+class Receipt {
+    var itemList: [Item] = []
+    
+    func items() -> [Item] {
+        return self.itemList
+    }
+    
+    func output() -> String {
+        var output = "Receipt:\n"
+        
+        for item in itemList {
+            output += "\(item.name): $\(Double(item.price()) / 100)\n"
+        }
+        return output + """
+        ------------------
+        TOTAL: $\(Double(self.total()) / 100)
+        """
+    }
+    
+    func total() -> Int {
+        var total = 0
+        for item in itemList {
+            total += item.price()
+        }
+        return total
+    }
+}
 
-class Register {}
+class Register {
+    var receipt = Receipt()
+    var totalPrice = 0
+    
+    func scan(_ item: Item) {
+        receipt.itemList.append(item)
+        totalPrice += item.price()
+    }
+    
+    func subtotal() -> Int {
+        return self.totalPrice
+    }
+    
+    func total() -> Receipt {
+        let receiptCopy = Receipt()
+        receiptCopy.itemList += receipt.items()
+        receipt = Receipt()
+        return receiptCopy
+    }
+}
 
 class Store {
     let version = "0.1"
